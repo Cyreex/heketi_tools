6fa5df39fe1d9f902127b1bd7e373845d04066ed
Artem Romanchik <a.romanchik@gmail.com>
2019-11-12 11:59:06 +0300

    Update add-lost-volume-to-heketi-db.sh

--- add-lost-volume-to-heketi-db.sh
+++ add-lost-volume-to-heketi-db.sh
@@ -1,38 +1,43 @@
-#С каким волумом работаем, его размер и производные от них
-export volume_name=vol_16c8b695505950ccc604e60e68c9de37
-export volume_size=2
+if [[ ${1} != "vol_"*  ]]; then
+  echo "You enter not correct volume name!"
+  echo "Example:"
+  echo "add-lost-volume-to-heketi-db.sh vol_16c8b695505950ccc604e60e68c9de37"   
+  exit 0
+fi 
+
+set -e
+
+export volume_name=$1
 
 input_json=before.json
 kubectl cp -n glusterfs glusterfs-heketi-0:var/lib/heketi/$input_json ./
 
-export volume_size_bytes=$(($volume_size*1048576))
 export volume_id=$(cut -d_ -f2 <<< $volume_name)
 
-#Генерируем GID волума 
+#Generate GID for a volume
 maxGid=$(grep gid $input_json | sort | tail -n 1 | awk '{print $2}' | cut -d, -f1)
 export volume_gid=$((maxGid+1))
 
-#Выцепляем ID кластера из хекети
+#Get the cluster ID from heketi
 export cluster_id=$(kubectl exec -n glusterfs glusterfs-heketi-0 -c heketi -- heketi-cli cluster list | tail -n1 | awk '{print $1}' | cut -d: -f2)
 
+#Get all gluster nodes registered in Heketi
 nodes=$(kubectl exec -n glusterfs glusterfs-heketi-0 -c heketi -- heketi-cli node list | awk '{print $1}' | cut -d: -f2)
-echo $nodes
-#Объявляем два массива для айпишников и айдишников нод в хекети
-declare -A node_ip=()
-declare -A node_heketi_id=()
 
-#counter
+declare -A node_ip=()
 counter=1
 
 for node in $nodes; do
   node_ip[$node]=$(kubectl exec -n glusterfs glusterfs-heketi-0 -c heketi -- heketi-cli node info $node | grep "Storage Hostname:" | awk '{print $3}')
   export declare "gluster_host_ip_$counter=${node_ip[$node]}"
-  #node_heketi_id Используется при добавлении айдишников томов в deviceentries (последний шаг)
-  node_heketi_id[$node]=$(kubectl exec -n glusterfs glusterfs-heketi-0 -c heketi -- heketi-cli node info $node | grep ^Id: | awk '{print $1}' | cut -d: -f2)
   counter=$((counter+1))
 done
 
-bricks=$(kubectl exec -n glusterfs $(kubectl get po -n glusterfs -l=name=glusterfs-gluster -o jsonpath='{.items[0].metadata.name}') gluster v info $volume_name | grep Brick[123] | awk '{print $2}')
+gluster_pod=$(kubectl get po -n glusterfs -l=name=glusterfs-gluster -o jsonpath='{.items[0].metadata.name}')
+
+bricks=$(kubectl exec -n glusterfs $gluster_pod gluster v info $volume_name | grep Brick[123] | awk '{print $2}')
+export volume_size=$(kubectl exec -n glusterfs $gluster_pod gluster v status $volume_name detail | grep "Total Disk Space" -m1 | awk '{print $5}' | cut -d. -f1)
+export volume_size_bytes=$(($volume_size*1048576))
 
 counter=1
 for brick in $bricks; do
@@ -52,15 +57,21 @@ envsubst < full.json > full-with-values.json
 
 #Add volume to Heketi DB (JSON)
 jq '.volumeentries += input.volume' $input_json full-with-values.json > tempDB.json
-jq '.brickentries += input.bricks' tempDB.json full-with-values.json > tempDB1.json
-jq --arg volume_id $volume_id --arg cluster_id $cluster_id  '.clusterentries[].Info.volumes[ .clusterentries[].Info.volumes | length ] +=  $volume_id' tempDB1.json > tempDB2.json
+jq '.brickentries += input.bricks' tempDB.json full-with-values.json > tempDB.tmp && mv tempDB.tmp tempDB.json
 
-jq --arg id $id --arg text xxx '.deviceentries | .[$id].Bricks[ .[$id].Bricks | length ] += $text' deviceentries.json
+jq --arg volume_id $volume_id --arg cluster_id $cluster_id  '.clusterentries[].Info.volumes[ .clusterentries[].Info.volumes | length ] +=  $volume_id' tempDB.js
+on > tempDB.tmp && mv tempDB.tmp tempDB.json
+
+deviceentries() {
+  jq --arg brick_id $1 --arg host_id $2 '.deviceentries | .[$host_id].Bricks[ .[$host_id].Bricks | length ] += $brick_id' tempDB.json > deviceentries.tmp
+  jq ".deviceentries = input" tempDB.json deviceentries.tmp > tempDB.tmp && mv tempDB.tmp tempDB.json && rm -f deviceentries.tmp
+}
 
 
-#deviceentries
+deviceentries $brick_id_1 $brick_vg_id_1
+deviceentries $brick_id_2 $brick_vg_id_2
+deviceentries $brick_id_2 $brick_vg_id_3
 
+mv tempDB.json after.json
 
-jq --arg volume_id $volume_id --arg cluster_id $cluster_id '.clusterentries | .[$cluster_id].Info.volumes[17] +=  $volume_id' tempDB1.json
-
-jq --arg volume_id $volume_id --arg cluster_id $cluster_id  '.clusterentries[].Info.volumes[ .clusterentries[].Info.volumes | length ] +=  $volume_id' tempDB1.json
+kubectl cp -n glusterfs ./after.json glusterfs-heketi-0:var/lib/heketi/
